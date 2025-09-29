b80370348fe759ffaf499719307a1edd3646019b
DESKTOP-0IS3ICF\Mert <mertmzzx@gmail.com>
2025-09-29 21:20:40 +0300

    Fixed path

--- CarPartsShop.Web/carpartsshop-admin/src/index.js
+++ CarPartsShop.Web/carpartsshop-admin/src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import "./styles/admin.css";
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
